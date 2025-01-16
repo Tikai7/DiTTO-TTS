cae43b45f317dc5df8f9d71468d5060246fe9441
Tikai7 <halimi.abdelkrim@outlook.fr>
2025-01-16 23:11:09 +0100

    issue with codec only on linux ??? not fixed yet

--- src/utils/Config.py
+++ src/utils/Config.py
@@ -14,7 +14,7 @@ class Config:
     MAX_AUDIO_DURATION = 20  # seconds
     
     # Text settings 
-    MAX_TOKEN_LENGTH = 128
+    MAX_TOKEN_LENGTH = 64
 
     # Model settings
     MODEL_NAME = "DiTTO-TTS"  # Replace with your model's name
@@ -31,11 +31,14 @@ class Config:
     BETAS = [0.9, 0.999]
 
     # Data settings 
-    TRAIN_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/train"
-    TEST_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/test"
-    DEV_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/dev"
+    # TRAIN_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/train"
+    # TEST_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/test"
+    # DEV_PATH = "/tempory/M2-DAC/UE_DEEP/AMAL/DiTTO-TTS/data/mls_french_opus/dev"
 
-
+    TRAIN_PATH = "C:/Cours-Sorbonne/M2/UE_DEEP/AMAL/Projet/data/mls_french_opus/mls_french_opus/train"
+    TEST_PATH = "C:/Cours-Sorbonne/M2/UE_DEEP/AMAL/Projet/data/mls_french_opus/mls_french_opus/test"
+    DEV_PATH = "C:/Cours-Sorbonne/M2/UE_DEEP/AMAL/Projet/data/mls_french_opus/mls_french_opus/dev"
+    
     @staticmethod
     def display():
         """Display the current configuration."""
